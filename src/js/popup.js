37560fba94bd7b5398e77188317a2351ee0d4414
Jonah Lysne <jonahboy03@gmail.com>
2022-11-11 18:09:39 -0800

    fixes fetch data

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,10 +1,10 @@
 // This function sends the data submitted through the form to the database.
 function sendToDatabase(data) {
-    fetch('http://127.0.0.1:8000/send-image', {
+    fetch('http://127.0.0.1:8000/send-image/', {
         method: 'POST',
         headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
+            'Accept': '*/*',
+            'Content-Type': 'applications/json',
         },
         body: JSON.stringify({
             image: data,
@@ -16,8 +16,22 @@ function sendToDatabase(data) {
                 console.log(data);
             }
         })
-        .catch((error) => {
-            alert('Error sendng request...')
+        .catch(() => {
+            alert('Error sending request...');
+        });
+}
+
+function testRequests() {
+    fetch('http://127.0.0.1:8000/test/', {
+        method: 'GET',
+    })
+        .then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                alert('test success');
+            }
+        })
+        .catch(() => {
+            alert('Error testing request...');
         });
 }
 
@@ -25,6 +39,13 @@ function sendToDatabase(data) {
 var form = document.getElementById('geoForm');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+
     var input = document.getElementById('input-field');
     sendToDatabase(input.text);
+});
+
+var testButton = document.getElementById('test-button');
+testButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    testRequests();
 });
